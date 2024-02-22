fd3ffee7db8fe6f8327a7dc78a784ff5cd7300c7
Georgios Kanellopoulos <kanellog@msc-linux-sls-008.scss.tcd.ie>
2024-02-22 20:11:27 +0000

    Improved script for calculating similarities in parallel

--- generate_similarity_graph_parallel.py
+++ generate_similarity_graph_parallel.py
@@ -6,20 +6,20 @@ import concurrent.futures
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def process_similarity(source_node, fp, file_lock):
+def process_similarity(source_node, source_data, fp, file_lock):
     start_time = time.perf_counter()
-    source_data = node_list[source_node]
     source_paper_id = source_data['id']
-    similarities = cosine_similarity(abstract_matrix[source_node], abstract_matrix)
-    sim_end_time = time.perf_counter()
-    print(f"Thread {threading.get_ident()}, source node {source_node}: Calculated similarities in {sim_end_time - start_time} seconds")
+    similarities = cosine_similarity(abstract_matrix[source_node], abstract_matrix)[0]
     with file_lock:
         for target_node, cos_sim in enumerate(similarities):
             target_paper_id = json.loads(papers[target_node])['paper_id']
             if target_paper_id != source_paper_id:
                 fp.write(f"{source_paper_id},{target_paper_id},{cos_sim}\n")
     end_time = time.perf_counter()
-    print(f"Thread {threading.get_ident()}, source node {source_node}: Total execution time is {end_time - start_time} seconds")
+    exec_time = end_time - start_time
+    print(f"Thread {threading.get_ident()}, source node {source_node}: \t Total execution time is {exec_time:.2f} seconds")
+
+print("Loading datasets.")
 
 # Load the citation data
 with open('./data/citation_relations.json', 'r') as f:
@@ -34,6 +34,8 @@ for paper_id in cite_data.keys():
 with open('./data/papers.SSN.jsonl', 'r') as f:
     papers = f.readlines()
 
+print("Creating TF-IDF vectors...")
+
 # Implement TF-IDF on the paper abstracts
 abstracts = []
 for paper in papers:
@@ -44,6 +46,8 @@ for paper in papers:
 vectorizer = TfidfVectorizer(stop_words='english')
 abstract_matrix = vectorizer.fit_transform(abstracts)
 
+print("Calculating similarities in parallel...")
+
 # Create a file lock to prevent race conditions
 file_lock = threading.Lock()
 
@@ -52,5 +56,7 @@ num_threads = 20
 with open('./similarities.csv', 'w') as fp:
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         # Create a new thread for each source node
-        threads = [executor.submit(process_similarity, source_node, fp, file_lock) for source_node in range(len(node_list))]
+        threads = [executor.submit(process_similarity, source_node, source_data, fp, file_lock) for source_node, source_data in enumerate(node_list)]
         concurrent.futures.wait(threads)
+
+print("Finished.")
